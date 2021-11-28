7fd8b658ae33d42dea0287fdc2c4db70955bace6
gabriel.caetano <gabriel.silva@khomp.com>
2021-11-28 18:59:26 -0300

    feat(express): inicia a implementação do controle de roteamento

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,10 +1,16 @@
 import log from "../log";
 import server from "../server";
+import { HTTP_METHODS } from "../server/data";
 
 import * as Types from "./types";
 
 class Express implements Types.Express {
   private server = server;
+
+  private routes: Array<{
+    method: string;
+    path: string;
+  }> = [];
 
   listen = (port: number, callback?: () => void): void => {
     try {
@@ -12,6 +18,29 @@ class Express implements Types.Express {
     } catch (error) {
       log.error("Express", "listen", error);
     }
+  };
+
+  get = (route: string): void => {
+    this.addRoute(route, HTTP_METHODS.GET);
+  };
+
+  put = (route: string): void => {
+    this.addRoute(route, HTTP_METHODS.PUT);
+  };
+
+  post = (route: string): void => {
+    this.addRoute(route, HTTP_METHODS.POST);
+  };
+
+  delete = (route: string): void => {
+    this.addRoute(route, HTTP_METHODS.DELETE);
+  };
+
+  private addRoute = (path: string, method: string): void => {
+    this.routes.push({
+      method,
+      path,
+    });
   };
 }
 
