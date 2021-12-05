d8b8f27cde944c302231ad93b40b0e9b218721d8
gabriel.caetano <gabriel.silva@khomp.com>
2021-12-05 17:01:49 -0300

    feat(express - types): implementa os tipos de request, responde e handler

--- src/express/types.ts
+++ src/express/types.ts
@@ -1,3 +1,10 @@
+import { IncomingMessage, ServerResponse } from "../server";
+
 export interface Express {
   listen: (port: number, callback?: () => void) => void;
 }
+
+type Request = IncomingMessage;
+type Response = ServerResponse;
+
+export type ExpressHandler = (req: Request, res: Response) => void;
