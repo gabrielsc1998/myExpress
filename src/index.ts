d54e1b180491c090690b536f5b7e8be3014790eb
gabriel.caetano <gabriel.silva@khomp.com>
2021-12-30 15:52:08 -0300

    feat-refact(index - app): implementa e refatora os endpoints de testes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import app from "./express";
 
-app.get("/x", (req, res) => res.end(`${req.method}`));
+app.get("/x", (req, res) => {
+  res.status(201);
+  res.setHeader("teste", "123");
+  res.send(`${req.method} - ${req.url}`);
+});
+app.get("/x2", (req, res) => res.send(`${req.method} - ${req.url}`));
+app.get("/x1", (req, res) => res.send(`${req.method} - ${req.url}`));
+
+app.post("/teste", (req, res) => {
+  // console.log(req);
+  console.log(req.body);
+  // const { teste } = req.body;
+  res.send(req.body);
+});
 
 app.listen(8000, () => {
   console.log("testando servidor - 8000");
